00f65181f90fa84408be4adb7dbf0cc5a1263d3d
U-CORP\swright <swright@V07C29D04123.corp.intuit.net>
2015-02-08 12:38:35 -0600

    validation working in the first part

--- src/multtable.php
+++ src/multtable.php
@@ -3,8 +3,28 @@
 
 The max multiplicand is: <?php echo $_GET["max-multiplicand"]; ?><br>
 The min multiplicand is: <?php echo $_GET["min-multiplicand"]; ?><br>
-The min multiplicand is: <?php echo $_GET["min-multiplier"]; ?><br>
-The max multiplicand is: <?php echo $_GET["max-multiplier"]; ?><br>
-
+The min multiplier is: <?php echo $_GET["min-multiplier"]; ?><br>
+The max multiplier is: <?php echo $_GET["max-multiplier"]; ?><br>
+<?php $maxcand = $_GET["max-multiplicand"];?>
+<?php $mincand = $_GET["min-multiplicand"];?>
+<?php $minmult = $_GET["min-multiplier"];?>
+<?php $maxmult = $_GET["max-multiplier"];?>
+ 
+<?php echo $maxcand; ?><br>
+<?php echo $mincand; ?><br>
+<?php echo $minmult; ?><br>
+<?php echo $maxmult; ?><br>
+<?php
+if ($mincand > $maxcand)
+{
+	echo 'The minimum multiplicand entered is greater than the maximum multiplicand entered';
+}
+?>
+<?php
+if ($minmult > $maxmult)
+{
+	echo 'The minimum multiplier entered is greater than the maximum multiplier entered';
+}
+?>
 </body>
 </html> 
