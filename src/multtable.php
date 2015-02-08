3b0531682e815423107812bf31544d0cfeb02d87
U-CORP\swright <swright@V07C29D04123.corp.intuit.net>
2015-02-08 10:09:20 -0600

    add src directory

---
+++ src/multtable.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+The max multiplicand is: <?php echo $_GET["max-multiplicand"]; ?><br>
+The min multiplicand is: <?php echo $_GET["min-multiplicand"]; ?><br>
+The min multiplicand is: <?php echo $_GET["min-multiplier"]; ?><br>
+The max multiplicand is: <?php echo $_GET["max-multiplier"]; ?><br>
+
+</body>
+</html> 
