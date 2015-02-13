1cbda67b976db6311cf34f361df5cac92417505a
Stephen Wright <wrighste@onid.oregonstate.edu>
2015-02-13 05:47:53 -0600

    login work

--- src/content1.php
+++ src/content1.php
@@ -3,12 +3,25 @@
 <?php
     if(empty($_POST['username']))
     {
-         echo "UserName is empty!";
+         echo "A username must be entered. Click here to return to the login screen";
+		 $callingserver = $_SERVER['HTTP_REFERER'];
+		 echo $callingserver;
+
     } else
 	{
-		$username = trim($_POST['username']);    
-		echo "Welcome $username! ";
+		
+		session_start();
+		session_name(($_POST['username']));
+		if (empty($_SESSION['count'])) {
+			$_SESSION['count'] = 1;
+		} else {
+			$_SESSION['count']++;
+			}
 	}
 ?>	
+		<p>
+		Hello visitor, you have seen this page <?php echo $_SESSION['count']; ?> times.
+		</p>
+
 </body>
 </html> 
